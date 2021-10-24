759fba67e65299a747bbb4d18416cd92826f21e9
Antonio Ochoa <antonhub00@gmail.com>
2021-10-24 01:09:47 -0500

    ADD: User entity

---
+++ entities/user.ts
@@ -0,0 +1,30 @@
+import { validateEmptyString } from "./utils";
+
+class User {
+  private _username: string;
+  private _password: string;
+
+  constructor(username: string, password: string) {
+    this.validateEmptyUsername(username);
+    this.validateEmptyPassword(password);
+
+    this._username = username;
+    this._password = password;
+  }
+
+  public get username(): string {
+    return this._username;
+  }
+
+  public get password(): string {
+    return this._password;
+  }
+
+  private validateEmptyUsername(value: string) {
+    validateEmptyString(value, "username");
+  }
+
+  private validateEmptyPassword(value: string) {
+    validateEmptyString(value, "password");
+  }
+}
