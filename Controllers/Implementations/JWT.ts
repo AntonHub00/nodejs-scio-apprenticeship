9183f6dc46c6712b407506a33cf5e7a71ee7e86c
Antonio Ochoa <antonhub00@gmail.com>
2021-10-24 18:22:13 -0500

    ADD: JWT implementation added

---
+++ Controllers/Implementations/JWT.ts
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import IJWT from "../../UseCases/User/Contracts/IJWT";
+
+class JWT implements IJWT {
+  private secret: string;
+
+  constructor() {
+    const secret = process.env.JWT_SECRET;
+
+    if (secret == null) throw new Error("JWT secret not set");
+
+    this.secret = secret;
+  }
+
+  public generate(username: string): string {
+    return jwt.sign({ username }, this.secret, { expiresIn: "1h" });
+  }
+
+  public validate(token: string) {
+    try {
+      const decoded = jwt.verify(token, this.secret) as jwt.JwtPayload;
+      return decoded.username;
+    } catch (error) {
+      return null;
+    }
+  }
+}
