985ff54e11495b5f4f9a1cce174ea5ae685f7165
Antonio Ochoa <antonhub00@gmail.com>
2021-10-24 20:59:27 -0500

    CHANGE: External services moved to Infrastructure directory

--- src/Infrastructure/Controllers/index.ts
+++ src/Infrastructure/Controllers/index.ts
@@ -3,8 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import "../DB/DBConnection";
-
-import JWT from "../JWT/JWT";
-import PasswordHasher from "../PasswordHasher/PasswordHasher";
+import "../JWT/JWT";
+import "../PasswordHasher/PasswordHasher";
 
 console.log("WORKING!");
