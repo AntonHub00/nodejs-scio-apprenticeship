664ba208185c849b63657de5cb676994c6dc4dad
Antonio Ochoa <antonhub00@gmail.com>
2021-10-24 20:17:00 -0500

    ADD: Database connection

---
+++ Controllers/Common/DBConnection.ts
@@ -0,0 +1,30 @@
+import { createConnection } from "typeorm";
+
+const dbConfigVariables = {
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+};
+
+const dbConfigIsValid = Object.values(dbConfigVariables).every(
+  (variable) => variable != null
+);
+
+if (!dbConfigIsValid) throw new Error("Database configuration not set");
+
+if (isNaN(dbConfigVariables.port))
+  throw new Error("Database port must be a number");
+
+createConnection({
+  type: "mssql",
+  host: dbConfigVariables.host,
+  database: dbConfigVariables.database,
+  port: dbConfigVariables.port,
+  username: dbConfigVariables.user,
+  password: dbConfigVariables.password,
+  options: {
+    encrypt: false,
+  },
+});
--- Controllers/index.ts
+++ Controllers/index.ts
@@ -2,5 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+import "../Controllers/Common/DBConnection";
+
 import JWT from "./Implementations/JWT";
 import PasswordHasher from "./Implementations/PasswordHasher";
