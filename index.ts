31552227a6a0a909ff930b56cb4fc17d51bf2cf4
Antonio Ochoa <antonhub00@gmail.com>
2021-10-27 09:01:55 -0500

    ADD: Actor controller with create user implementation

--- index.ts
+++ index.ts
@@ -15,6 +15,10 @@ const startServer = async () => {
     "./src/Infrastructure/Controllers/User/UserController"
   );
 
+  const { default: ActorController } = await import(
+    "./src/Infrastructure/Controllers/Actor/ActorController"
+  );
+
   const port = process.env.APP_PORT;
   if (port == null) throw new Error("App port not set");
 
@@ -22,6 +26,7 @@ const startServer = async () => {
 
   app.use(express.json());
   app.use("/api/user", new UserController().router);
+  app.use("/api/actor", new ActorController().router);
 
   app.listen(port, () =>
     console.log(`Server listening on http://localhost:${port}`)
---
+++ src/Infrastructure/Controllers/Actor/ActorController.ts
@@ -0,0 +1,42 @@
+import { Request, Response, Router } from "express";
+import { createActorUseCase } from "../../../DependencyInjection/ActorDependencyInjection";
+
+export default class ActorController {
+  private _router: Router;
+
+  constructor() {
+    this._router = Router();
+
+    this.createActor = this.createActor.bind(this);
+
+    this.router.post("/", this.createActor);
+  }
+
+  public get router(): Router {
+    return this._router;
+  }
+
+  public async createActor(req: Request, res: Response) {
+    const firstName: string = req.body.firstName;
+    const lastName: string = req.body.lastName;
+    const yearOfBirth: number = req.body.yearOfBirth;
+
+    if (!(firstName || lastName || yearOfBirth))
+      res.status(400).send({
+        error: "You must provide firstName, lastName and yearOfBirth",
+      });
+
+    try {
+      const dbActor = await createActorUseCase.createActor({
+        firstName,
+        lastName,
+        yearOfBirth,
+      });
+
+      res.status(201).send(dbActor);
+    } catch (error) {
+      const e = error as Error;
+      res.status(400).send({ error: e });
+    }
+  }
+}
