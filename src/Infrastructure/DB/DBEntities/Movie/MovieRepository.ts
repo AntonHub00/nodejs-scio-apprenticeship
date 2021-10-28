f4a8971199032cfd7bf1fabc74f44d499468f3d5
Antonio Ochoa <antonhub00@gmail.com>
2021-10-28 08:51:55 -0500

    ADD: Add existing actors to movie implementation in movie repository

--- src/Infrastructure/DB/DBEntities/Movie/MovieRepository.ts
+++ src/Infrastructure/DB/DBEntities/Movie/MovieRepository.ts
@@ -2,19 +2,22 @@ import { getRepository, Repository } from "typeorm";
 import IMovieCreateDTO from "../../../../Domain/UseCases/Movie/Contracts/IMovieCreateDTO";
 import IMovieDTO from "../../../../Domain/UseCases/Movie/Contracts/IMovieDTO";
 import IMovieRepository from "../../../../Domain/UseCases/Movie/Contracts/IMovieRepository";
+import ActorDBEntity from "../Actor/ActorDBEntity";
 import MovieDBEntity from "./MovieDBEntity";
 
 export default class MovieRepository implements IMovieRepository {
-  private _repository: Repository<MovieDBEntity>;
+  private _movieRepository: Repository<MovieDBEntity>;
+  private _actorRepository: Repository<ActorDBEntity>;
 
   constructor() {
-    this._repository = getRepository(MovieDBEntity);
+    this._movieRepository = getRepository(MovieDBEntity);
+    this._actorRepository = getRepository(ActorDBEntity);
   }
 
   public async createMovie(movie: IMovieCreateDTO): Promise<IMovieDTO> {
-    const newMovie = this._repository.create(movie);
+    const newMovie = this._movieRepository.create(movie);
 
-    await this._repository.save(newMovie);
+    await this._movieRepository.save(newMovie);
 
     return {
       id: newMovie.id,
@@ -25,7 +28,7 @@ export default class MovieRepository implements IMovieRepository {
   }
 
   public async findMovieById(id: number): Promise<IMovieDTO | null> {
-    const movie = await this._repository.findOne(id);
+    const movie = await this._movieRepository.findOne(id);
 
     if (movie == null) return null;
 
@@ -42,7 +45,7 @@ export default class MovieRepository implements IMovieRepository {
 
     if (dbMovie == null) throw new Error("movie does not exist");
 
-    await this._repository.update(id, movie);
+    await this._movieRepository.update(id, movie);
   }
 
   public async deleteMovieById(id: number): Promise<void> {
@@ -50,11 +53,11 @@ export default class MovieRepository implements IMovieRepository {
 
     if (dbMovie == null) throw new Error("movie does not exist");
 
-    await this._repository.delete(id);
+    await this._movieRepository.delete(id);
   }
 
   public async getAllMovies(): Promise<IMovieDTO[]> {
-    const dbMovies = await this._repository.find();
+    const dbMovies = await this._movieRepository.find();
 
     const movies = dbMovies.map((dbMovie) => ({
       id: dbMovie.id,
@@ -64,5 +67,26 @@ export default class MovieRepository implements IMovieRepository {
     }));
 
     return movies;
+  }
+
+  public async addExistingActorsToMovie(movieId: number, actorIds: number[]) {
+    const dbMovie = await this._movieRepository.findOne(movieId);
+
+    if (dbMovie == null) throw new Error("movie does not exist");
+
+    let actors: ActorDBEntity[] = [];
+
+    for (const actorId of actorIds) {
+      const actor = await this._actorRepository.findOne(actorId);
+
+      if (actor == null)
+        throw new Error(`actor with id ${actorId} does not exist`);
+
+      actors = [...actors, actor];
+    }
+
+    dbMovie.actors = actors;
+
+    this._movieRepository.save(dbMovie);
   }
 }
